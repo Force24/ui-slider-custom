177670a4dc204e8449b05e928673d34084a6b5d3
Brennan Jenkins <brennan.jenkins@force24.co.uk>
2019-06-11 10:03:41 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+﻿$( function() {
+    var handle = $( ".custom-handle" );
+    var handle2 = $( ".custom-handle-2" );
+    $( ".slider" ).slider({
+        min: 0,
+        max: 100,
+        value: 50,
+        animate:"slow",
+        create: function() {
+            handle.text( $( this ).slider( "value" ) );
+        },
+        slide: function( event, ui ) {
+            handle.text( ui.value );
+            $('input[name=employees]').val(ui.value);
+        }
+    });
+    $( ".slider-2" ).slider({
+        min: 0,
+        max: 50000,
+        step: 100,
+        value: 25000,
+        animate:"slow",
+        create: function() {
+            handle2.text( "25k" );
+        },
+        slide: function( event, ui ) {
+            handle2.text( ui.value / 1000 + "k" );
+            $('input[name=followers]').val(ui.value);
+        }
+    });
+  } );
