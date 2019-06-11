a531dbd60cb79c537b3945b8f08817662e83155b
Brennan Jenkins <brennan.jenkins@force24.co.uk>
2019-06-11 17:18:44 +0100

    added calculations and results boxes

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
     var handle2 = $( ".custom-handle-2" );
     $( ".slider" ).slider({
         min: 0,
-        max: 100,
-        value: 50,
+        max: 250,
+        value: 125,
         animate:"slow",
         create: function() {
             handle.text( $( this ).slider( "value" ) );
@@ -28,4 +28,50 @@
             $('input[name=followers]').val(ui.value);
         }
     });
-  } );
+    var frm = $('.CalcReturn');
+    frm.submit(function (e) {
+        $.ajax({
+            type: frm.attr('method'),
+            url: frm.attr('action'),
+            data: frm.serialize(),
+            success: function (data) {
+                function formatNumber(num) {
+                    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+                }
+                var employees = parseInt($('input[name=employees]').val());
+                var followers = parseInt($('input[name=followers]').val());
+                var reach = ((employees*1000)+followers);
+                var package
+                var clicks
+                var roi
+                if(employees <= 5){
+                    package = 120
+                }
+                else if(employees >= 6 && employees <= 10){
+                    package = 199
+                }
+                else if(employees >= 11 && employees <= 20){
+                    package = 350
+                }
+                else if(employees >= 21 && employees <= 49){
+                    package = 599
+                }
+                else if(employees >= 50 && employees <= 100){
+                    package = 899
+                }
+                else if(employees >= 101 && employees <= 249){
+                    package = 1250
+                }
+                else if(employees >= 250){
+                    package = 2250
+                }
+                clicks = (reach/100)*2;
+                roi = ((clicks - package)/package) * 100
+                $('#calcReach').html(formatNumber(reach));
+                $('#calcROI').html(Math.round(roi));
+                $('#calcPSC').html(formatNumber(clicks * 12));
+            }
+        });
+        e.preventDefault();
+    });
+  });
